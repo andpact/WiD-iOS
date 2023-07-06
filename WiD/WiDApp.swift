bcee02760a07ac36688e6ad16d92c489bd28aec5
andpact <andpact@gmail.com>
2023-07-06 15:48:40 +0900

    first commit

---
+++ WiD/WiDApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiDApp.swift
+//  WiD
+//
+//  Created by jjkim on 2023/07/06.
+//
+
+import SwiftUI
+
+@main
+struct WiDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
